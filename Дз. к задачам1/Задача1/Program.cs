6f64acf3951db4c7c9e7234d1454d58d4c5b0d3c
Maria <mas04081997@mail.ru>
2023-06-20 13:01:56 +0300

    Fils

---
+++ Дз. к задачам1/Задача1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Home_1. Составить программу вывода на экран в одну
+//  строку трех любых чисел с двумя пробелами между ними.
+
+
+// Console.WriteLine(" 5  10 15 ");
+
+
+//Console.Write(5 + " 11 1025");
+Console.Write("5 11 " + 1025);
+// Console.Write(5 + " " + 11 + " " + 1025);
+// Console.WriteLine("5 11 1025");
+// Console.WriteLine(5 + " " + 11 + " " + 1025);
+// Console.Write("5 11 1025");
